6b45eb6e1720f8eb44ecfd8730b9fcee6d7b663a
John Deadman <xSomoy@tutanota.com>
2023-01-24 12:04:48 +0600

    e1.4

--- Book-Think_Python/e1-4.py
+++ Book-Think_Python/e1-4.py
@@ -1,8 +1,12 @@
 total_time = (43*60) + 30 # in seconds
 total_mile = 10/1.61
 
-avg_time_per_mile = total_time / total_mile
+avg_time_per_mile = total_time / total_mile 
+avg_speed_in_miles_hour = total_mile / (total_time / (60*60))
 
-print('Avarage Time Per Mile (In Seconds): ' + "{:.2f}".format(avg_time_per_mile)) # in seconds
+print('Average Time Per Mile (In Seconds): ' + "{:.2f}".format(avg_time_per_mile)) # in seconds
+print('Average Miles Per Hour : ' + '{:.2f}'.format(avg_speed_in_miles_hour))
+
+
 
 
