2b3dd7590792e646888100735d1c8097b16d665c
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-01 23:23:05 +0600

    CIP3-W6-w

---
+++ Stanford_Code-In-Place_3/WEEK06/dictionary.py
@@ -0,0 +1,18 @@
+# Create an empty dictionary
+my_dict = {}
+
+# Assign values to keys
+my_dict['key1'] = 'value1'
+my_dict['key2'] = "a"
+my_dict['key3'] = 'value3'
+
+print(my_dict)
+
+print(type(my_dict['key2']))
+
+
+
+# List Lenth 
+
+names = [1,2,3,4]
+print(len(names))
