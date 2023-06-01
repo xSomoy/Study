3fd886f00a1e978394300fee5041f352fbbc2835
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-01 23:34:26 +0600

    CIP3-W6-w

--- Stanford_Code-In-Place_3/WEEK06/dictionary.py
+++ Stanford_Code-In-Place_3/WEEK06/dictionary.py
@@ -1,7 +1,8 @@
-# Create an empty dictionary
+# John Deadman
+
 my_dict = {}
 
-# Assign values to keys
+# Assign values to the dictionary
 my_dict['key1'] = 'value1'
 my_dict['key2'] = "a"
 my_dict['key3'] = 'value3'
