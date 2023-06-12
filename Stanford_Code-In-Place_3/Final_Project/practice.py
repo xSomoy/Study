0e4d2b970dd44b65083660d22d5ee9b959a07a6a
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-13 00:27:01 +0600

    CIP3 final project- w

--- Stanford_Code-In-Place_3/Final_Project/practice.py
+++ Stanford_Code-In-Place_3/Final_Project/practice.py
@@ -7,7 +7,7 @@ CANVAS_HEIGHT = 800
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     # canvas.create_rectangle(20,20,100,100,"blue")
-    canvas.create_image_with_size(0, 0, 100, 120, "Karel.png")
+    canvas.create_image_with_size(0, CANVAS_HEIGHT-120, 100, 120, "Karel.png")
 
 
 if __name__ == '__main__':
