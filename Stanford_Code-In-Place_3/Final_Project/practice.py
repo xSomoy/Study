518aecbc48daf0ae9bd1103d9ddd7473fab2c584
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-12 23:59:06 +0600

    CIP3 final project- for some reason image doesn't work here

--- Stanford_Code-In-Place_3/Final_Project/practice.py
+++ Stanford_Code-In-Place_3/Final_Project/practice.py
@@ -7,7 +7,7 @@ CANVAS_HEIGHT = 800
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     # canvas.create_rectangle(20,20,100,100,"blue")
-    canvas.create_image(0, 550, "Karel.png")
+    canvas.create_image(0, 550, "../Karel.png")
     a = input()
 
 
