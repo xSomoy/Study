043d3e5def55a795b771dd5bf1b7e1e3ca565776
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-13 00:54:12 +0600

    CIP3 final project - Character Position

--- Stanford_Code-In-Place_3/Final_Project/practice.py
+++ Stanford_Code-In-Place_3/Final_Project/practice.py
@@ -1,13 +1,16 @@
+# Character Position
+
 from graphics import Canvas
 
 CANVAS_WIDTH = 600
-CANVAS_HEIGHT = 800
+CANVAS_HEIGHT = 500
 
 
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     # canvas.create_rectangle(20,20,100,100,"blue")
-    canvas.create_image_with_size(0, CANVAS_HEIGHT-120, 100, 120, "Karel.png")
+    canvas.create_image_with_size(
+        100, CANVAS_HEIGHT-120, 100, 120, "Karel.png")
 
 
 if __name__ == '__main__':
