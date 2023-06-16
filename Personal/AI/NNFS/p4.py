e4b627a35ae7c8e8bc5f91b6316b710e1aecc937
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 00:40:28 +0600

    p4

--- Personal/AI/NNFS/p4.py
+++ Personal/AI/NNFS/p4.py
@@ -9,9 +9,11 @@ X = [[1, 2, 3, 2.5],
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = np.random.rand(n_inputs, n_neurons)
+        self.weights = np.random.randn(n_inputs, n_neurons)
     def forward(self):
         pass
+
+print(np.random.randn(4,3))
 
 
 # ----------------------------------------- #
