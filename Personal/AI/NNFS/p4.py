563bbdc44a56038ba59afbfdd239573d4715eb2c
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 00:21:57 +0600

    p4

--- Personal/AI/NNFS/p4.py
+++ Personal/AI/NNFS/p4.py
@@ -4,7 +4,11 @@ X = [[1, 2, 3, 2.5],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5,2.7,3.3,-0.8]]
 
-
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
 
 
 
