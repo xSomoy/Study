0a2d87225fa027d3f963a601382378d29805c6fe
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-16 23:52:05 +0600

    p4

---
+++ Personal/AI/NNFS/p4.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5,2.7,3.3,-0.8]]
+
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+
+outputs = np.dot(weights, inputs) + biases
+
+print(outputs)
+
