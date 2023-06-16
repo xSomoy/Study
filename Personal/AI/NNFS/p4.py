a4a2c5ce8d143a6026102a5f1d1a8db2c6874c02
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 00:50:30 +0600

    p4

--- Personal/AI/NNFS/p4.py
+++ Personal/AI/NNFS/p4.py
@@ -10,7 +10,7 @@ X = [[1, 2, 3, 2.5],
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros(1, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
