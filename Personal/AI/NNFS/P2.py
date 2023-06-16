2d033440279eb83fef8d2f8eb4f5b73dcad03589
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-16 22:46:33 +0600

    p2

--- Personal/AI/NNFS/P2.py
+++ Personal/AI/NNFS/P2.py
@@ -1,7 +1,11 @@
 inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights1 = [0.5, -0.91, 0.26, -0.5]
+weights1 = [-0.26, -0.27, 0.17, 0.87]
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
-output = inputs[0]*weights[0] + inputs[1] * weights[1] + inputs[2]*weights[2]  + inputs[3]*weights[3] + bias
+output = inputs[0]*weights1[0] + inputs[1] * weights1[1] + inputs[2]*weights1[2]  + inputs[3]*weights1[3] + bias1
 
 print(output)
