3d2ee46967e0ac73704c0226ee3da43a877bdf11
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 00:22:41 +0600

    p4

--- Personal/AI/NNFS/p4.py
+++ Personal/AI/NNFS/p4.py
@@ -1,23 +1,16 @@
 import numpy as np
 
 X = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5,2.7,3.3,-0.8]]
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
 
 class Layer_Dense:
     def __init__(self):
         pass
+
     def forward(self):
         pass
-
-
-
-
-
-
-
-
-
 
 
 # ----------------------------------------- #
@@ -53,4 +46,3 @@ class Layer_Dense:
 # outputs = np.dot(np.array(weights).T, inputs) + biases #doesn't work
 
 # print(layer2_outputs)
-
