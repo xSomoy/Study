e73850ee0b654037d32ca8cb239471bba7975d24
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 00:10:34 +0600

    p4

--- Personal/AI/NNFS/p4.py
+++ Personal/AI/NNFS/p4.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# layer 1
 inputs = [[1, 2, 3, 2.5],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5,2.7,3.3,-0.8]]
@@ -14,15 +15,16 @@ biases = [2, 3, 0.5]
 
 weights2 = [[0.1, -0.14, 0.5],
            [-0.5, 0.12, -0.33],
-           [-0.44, -0.27, ]]
+           [-0.44, -0.27, -0.13 ]]
 
-biases2 = [2, 3, 0.5]
+biases2 = [-1, 2, -0.5]
 
 
-# outputs = np.dot(inputs, np.array(weights).T) + biases
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 
-outputs = np.dot(weights, np.array(inputs).T) + biases #doesn't work
+# outputs = np.dot(weights, np.array(inputs).T) + biases #does work
 # outputs = np.dot(np.array(weights).T, inputs) + biases #doesn't work
 
-print(outputs)
+print(layer1_outputs)
 
