9606009490fca9f458ef90bac4507a7bd378f39e
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-16 23:23:07 +0600

    p3

--- Personal/AI/NNFS/p3.py
+++ Personal/AI/NNFS/p3.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
 
 
-outputs = np.dot(weights, inputs) + bias
+outputs = np.dot(weights, inputs) + biases
 
 print(outputs)
 
