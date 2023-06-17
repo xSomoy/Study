76f92e2ce84b823f1fa004888812d25ad89c9248
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 08:20:06 +0600

    p6-1

--- Personal/AI/Neural_Networks_from_Scratch/p6-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p6-1.py
@@ -13,3 +13,12 @@ for output in layer_outputs:
     exp_values.append(E**output)
 
 print(exp_values)
+
+norm_base = sum(exp_values)
+norm_values = []
+
+for value in exp_values:
+    norm_values.append(value / norm_base)
+
+print(norm_values)
+print(sum(norm_values))
