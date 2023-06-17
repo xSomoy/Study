8fd582d5606cfa6cde3b72c27aa8deb316f78dd5
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 07:49:01 +0600

    p5

--- Personal/AI/Neural_Networks_from_Scratch/p5.py
+++ Personal/AI/Neural_Networks_from_Scratch/p5.py
@@ -1,10 +1,15 @@
 import numpy as np
 import nnfs
+
+from nnfs.datasets import spiral_data
+
 nnfs.init()
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
 
 
 class Layer_Dense:
