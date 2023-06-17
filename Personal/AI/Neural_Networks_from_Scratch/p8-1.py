85b3157652cfd10b999f913a20597e5d42a34cf0
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 12:06:30 +0600

    p8-1

--- Personal/AI/Neural_Networks_from_Scratch/p8-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p8-1.py
@@ -5,6 +5,9 @@ softmax_outputs = np.array([[0.7, 0.1, 0.2],
                             ])
 class_targets = [0, 1, 1]
 
-print(softmax_outputs[[0, 1, 2], class_targets])
+# print(softmax_outputs[[0, 1, 2], class_targets])
 
-print(softmax_outputs[range(len(softmax_outputs)), class_targets])
+neg_log = np.log(softmax_outputs[range(len(softmax_outputs)), class_targets])
+
+average_loss = np.mean(neg_log)
+print(average_loss)
