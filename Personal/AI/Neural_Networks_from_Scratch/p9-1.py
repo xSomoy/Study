448df87f9a1f94c3f88fe832a53a52061345c35b
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 13:29:43 +0600

    p9-1

--- Personal/AI/Neural_Networks_from_Scratch/p9-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p9-1.py
@@ -75,4 +75,18 @@ for iteration in range(100000):
     dense2.weights = 0.05 * np.random.randn(3, 3)
     dense2.biases = 0.05 * np.random.randn(1, 3)
 
-    dense1.forward(x)
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    loss = loss_function.calculate(activation2.output, y)
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions == y)
+    if loss < lowset_loss:
+        print('New set of weights found iteration:',
+              iteration, 'loss:', loss, 'acc:', accuracy)
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
