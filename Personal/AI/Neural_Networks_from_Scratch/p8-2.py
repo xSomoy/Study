520331d85d97b3f974d537e56cb37791a7485b90
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 12:19:25 +0600

    p8-2

--- Personal/AI/Neural_Networks_from_Scratch/p8-2.py
+++ Personal/AI/Neural_Networks_from_Scratch/p8-2.py
@@ -27,6 +27,24 @@ class Activation_Softmax:
         self.output = probabilities
 
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output. y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true)
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            corrent_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            corrent_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
