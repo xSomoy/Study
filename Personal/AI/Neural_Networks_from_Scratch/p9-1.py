459cc4f805ed6d56fe6dedd53c6c15534f4da2dc
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 13:21:59 +0600

    p9-1

--- Personal/AI/Neural_Networks_from_Scratch/p9-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p9-1.py
@@ -1,7 +1,51 @@
+from nnfs.datasets import spiral_data
+import numpy as np
 import matplotlib.pyplot as plt
 import nnfs
 from nnfs.datasets import vertical_data
 nnfs.init()
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:  # Rectified Lieaner Unit
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            corrent_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            corrent_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(corrent_confidences)
+        return negative_log_likelihoods
+
 
 X, y = vertical_data(samples=100, classes=3)
 
@@ -18,5 +62,11 @@ loss_function = Loss_CategoricalCrossentropy()
 lowset_loss = 9999999
 best_dense1_weights = dense1.weights.copy()
 best_dense1_biases = dense1.biases.copy()
-best_dense1_weights = dense1.weights.copy()
-best_dense1_weights = dense1.weights.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+
+for iteration in range(100000):
+    dense1.weights = 0.05 * np.random.randn(2, 3)
+    dense1.biases = 0.05 * np.random.randn(1, 3)
+    dense2.weights = 0.05 * np.random.randn(3, 3)
+    dense2.biases = 0.05 * np.random.randn(1, 3)
