f1097ab6fdb5b1e224e565c538b1cb911be6a9fb
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 08:16:38 +0600

    p6-1

--- Personal/AI/Neural_Networks_from_Scratch/p6-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p6-1.py
@@ -1,8 +1,17 @@
-import math
 # Softmax Activision Function
+import math
 
 layer_outputs = [4.8, 1.12, 2.385]
-# E = 2.71828182846
+
 E = math.e
 
+
+# E = 2.71828182846
 # print(E)
+
+exp_values = []
+for output in layer_outputs:
+    exp_values.append(E**output)
+
+
+print(exp_values)
