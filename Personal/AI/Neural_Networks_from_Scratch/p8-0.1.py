d36b9e335a17bf43df7ad87ef4fc8b0fb15a526e
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 12:48:35 +0600

    p8-1.0

--- Personal/AI/Neural_Networks_from_Scratch/p8-0.1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p8-0.1.py
@@ -70,7 +70,10 @@ print('Loss:', loss)
 
 # Calculate Accuracy
 
-softmax_outputs = np.array(activation2.output)
+softmax_outputs = np.array([[0.7, 0.2, 0.1],
+                            [0.5, 0.1, 0.4],
+                            [0.02, 0.9, 0.08]
+                            ])
 
 class_targets = [0, 1, 1]
 
