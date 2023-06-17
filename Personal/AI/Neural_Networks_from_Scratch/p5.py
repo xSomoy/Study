55e194d6a7a33c9e699c03e18750ae5ceda570eb
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 06:33:02 +0600

    p5

--- Personal/AI/Neural_Networks_from_Scratch/p5.py
+++ Personal/AI/Neural_Networks_from_Scratch/p5.py
@@ -7,18 +7,22 @@ X = [[1, 2, 3, 2.5],
      [-1.5, 2.7, 3.3, -0.8]]
 
 
-
-
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
 layer1.forward(X)
 # print('\nLayer 1 Outputs\n')
@@ -27,9 +31,6 @@ layer1.forward(X)
 layer2.forward(layer1.output)
 # print('\nLayer 2 Outputs\n')
 print(layer2.output)
-
-
-
 
 
 # inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
