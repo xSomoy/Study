395d822dfada5c427267057a1c9aab5b9f30ad51
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 08:38:17 +0600

    p6-1.1

---
+++ Personal/AI/Neural_Networks_from_Scratch/p6-1.1.py
@@ -0,0 +1,16 @@
+# Softmax Activision Function = Exponentiate > Normalize > Output
+import math
+import numpy as np
+import nnfs
+nnfs.init()
+
+layer_outputs = [4.8, 1.12, 2.385]
+
+E = math.e
+
+exp_values = np.exp(layer_outputs)
+
+norm_values = exp_values / np.sum(exp_values)
+
+print(norm_values)
+print(sum(norm_values))
