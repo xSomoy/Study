8229c29281d336422ffad155704f13d33f808ca1
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 11:45:55 +0600

    p7

--- Personal/AI/Neural_Networks_from_Scratch/p7.py
+++ Personal/AI/Neural_Networks_from_Scratch/p7.py
@@ -1,7 +1,20 @@
-# Logarithm Math
+
 import numpy as np
 import math
 
-b = 5.2
-print(np.log(b))
-print(math.e ** 1.6486586255873816)
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+loss = -(math.log(softmax_output[0])*target_output[0] +
+         math.log(softmax_output[1])*target_output[1] +
+         math.log(softmax_output[2])*target_output[2])
+
+print(loss)
+
+loss = -math.log(softmax_output[0])
+
+print(loss)
+
+# Logarithm Math
+# b = 5.2
+# print(np.log(b))
+# print(math.e ** 1.6486586255873816)
