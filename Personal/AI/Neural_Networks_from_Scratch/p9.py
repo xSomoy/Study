f6cb87be34c0f135482a5c87df246f60c8038cb0
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 14:13:18 +0600

    p9

--- Personal/AI/Neural_Networks_from_Scratch/p9.py
+++ Personal/AI/Neural_Networks_from_Scratch/p9.py
@@ -4,66 +4,71 @@ import nnfs
 from nnfs.datasets import spiral_data
 nnfs.init()
 
+X, y = spiral_data(samples=100, classes=3)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.show()
+
 
 # -----------------------------------------------------
 
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+# class Layer_Dense:
+#     def __init__(self, n_inputs, n_neurons):
+#         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+#         self.biases = np.zeros((1, n_neurons))
 
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-
-class Activation_ReLU:  # Rectified Lieaner Unit
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
+#     def forward(self, inputs):
+#         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-class Activation_Softmax:
-    def forward(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        self.output = probabilities
+# class Activation_ReLU:  # Rectified Lieaner Unit
+#     def forward(self, inputs):
+#         self.output = np.maximum(0, inputs)
 
 
-class Loss:
-    def calculate(self, output, y):
-        sample_losses = self.forward(output, y)
-        data_loss = np.mean(sample_losses)
-        return data_loss
+# class Activation_Softmax:
+#     def forward(self, inputs):
+#         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+#         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+#         self.output = probabilities
 
 
-class Loss_CategoricalCrossentropy(Loss):
-    def forward(self, y_pred, y_true):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
-
-        if len(y_true.shape) == 1:
-            corrent_confidences = y_pred_clipped[range(samples), y_true]
-        elif len(y_true.shape) == 2:
-            corrent_confidences = np.sum(y_pred_clipped * y_true, axis=1)
-        negative_log_likelihoods = -np.log(corrent_confidences)
-        return negative_log_likelihoods
+# class Loss:
+#     def calculate(self, output, y):
+#         sample_losses = self.forward(output, y)
+#         data_loss = np.mean(sample_losses)
+#         return data_loss
 
 
-X, y = spiral_data(samples=100, classes=3)
+# class Loss_CategoricalCrossentropy(Loss):
+#     def forward(self, y_pred, y_true):
+#         samples = len(y_pred)
+#         y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
 
-dense1 = Layer_Dense(2, 3)
-activation1 = Activation_ReLU()
-dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
+#         if len(y_true.shape) == 1:
+#             corrent_confidences = y_pred_clipped[range(samples), y_true]
+#         elif len(y_true.shape) == 2:
+#             corrent_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+#         negative_log_likelihoods = -np.log(corrent_confidences)
+#         return negative_log_likelihoods
 
-dense1.forward(X)
-activation1.forward(dense1.output)
 
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
+# X, y = spiral_data(samples=100, classes=3)
 
-print(activation2.output[:5])
+# dense1 = Layer_Dense(2, 3)
+# activation1 = Activation_ReLU()
+# dense2 = Layer_Dense(3, 3)
+# activation2 = Activation_Softmax()
 
-loss_fucntion = Loss_CategoricalCrossentropy()
-loss = loss_fucntion.calculate(activation2.output, y)
+# dense1.forward(X)
+# activation1.forward(dense1.output)
 
-print('Loss:', loss)
+# dense2.forward(activation1.output)
+# activation2.forward(dense2.output)
+
+# print(activation2.output[:5])
+
+# loss_fucntion = Loss_CategoricalCrossentropy()
+# loss = loss_fucntion.calculate(activation2.output, y)
+
+# print('Loss:', loss)
