f0a04191550548d5bb54a7eb00a40a982fc93ed2
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 12:59:39 +0600

    p9-1

--- Personal/AI/Neural_Networks_from_Scratch/p9-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p9-1.py
@@ -4,4 +4,5 @@ from nnfs.datasets import vertical_data
 nnfs.init()
 
 X, y = vertical_data(samples=100, classes=3)
-plt.setattr(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.show()
