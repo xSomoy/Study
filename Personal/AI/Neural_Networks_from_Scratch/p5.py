d17ae95f188fa9779d2d396ad2cc49f883613397
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 06:27:26 +0600

    p5

--- Personal/AI/Neural_Networks_from_Scratch/p5.py
+++ Personal/AI/Neural_Networks_from_Scratch/p5.py
@@ -7,6 +7,16 @@ X = [[1, 2, 3, 2.5],
      [-1.5, 2.7, 3.3, -0.8]]
 
 
+# inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+# output = []
+
+# for i in inputs:
+#     output.append(max(0,i))
+
+# print(output)
+
+
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -24,3 +34,4 @@ layer1.forward(X)
 layer2.forward(layer1.output)
 # print('\nLayer 2 Outputs\n')
 print(layer2.output)
+
