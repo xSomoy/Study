80530aa7505b1761a7c4217b8d10cffb092ccbf5
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 08:39:48 +0600

    p6-1.1

--- Personal/AI/Neural_Networks_from_Scratch/p6-1.1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p6-1.1.py
@@ -4,7 +4,9 @@ import numpy as np
 import nnfs
 nnfs.init()
 
-layer_outputs = [4.8, 1.12, 2.385]
+layer_outputs = [[4.8, 1.12, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
 
 E = math.e
 
