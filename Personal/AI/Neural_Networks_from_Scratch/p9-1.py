336a06f83c44a928286f73d607d188d0faa2aa3b
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-17 13:07:25 +0600

    p9-1

--- Personal/AI/Neural_Networks_from_Scratch/p9-1.py
+++ Personal/AI/Neural_Networks_from_Scratch/p9-1.py
@@ -4,5 +4,19 @@ from nnfs.datasets import vertical_data
 nnfs.init()
 
 X, y = vertical_data(samples=100, classes=3)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
-plt.show()
+
+# plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+# plt.show()
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+loss_function = Loss_CategoricalCrossentropy()
+
+lowset_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense1_weights = dense1.weights.copy()
+best_dense1_weights = dense1.weights.copy()
