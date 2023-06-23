12b7eff9f5ca50748cbbdf830f37a699a7de00a8
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-24 02:17:51 +0600

    ws1 -w

--- WebScraping/BeautifulSoup4/ws-1.py
+++ WebScraping/BeautifulSoup4/ws-1.py
@@ -3,4 +3,7 @@ from bs4 import BeautifulSoup
 with open("index.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
 
-print(doc.prettify())
+# print(doc.prettify())
+
+tag = doc.title
+print(tag)
