8068ae42accf3a3de9c45c5255e7ac8c75025591
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-24 02:32:39 +0600

    ws2 -w

--- WebScraping/BeautifulSoup4/ws-2.py
+++ WebScraping/BeautifulSoup4/ws-2.py
@@ -1,1 +1,9 @@
 from bs4 import BeautifulSoup
+import requests
+
+# HTML URL
+url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
+
+result = requests.get(url)
+
+print(result.text)
