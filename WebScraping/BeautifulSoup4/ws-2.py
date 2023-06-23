ec3e29c5a0d89be4f333d94889e50df0fe620af9
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-06-24 02:56:27 +0600

    ws2 -w

--- WebScraping/BeautifulSoup4/ws-2.py
+++ WebScraping/BeautifulSoup4/ws-2.py
@@ -5,5 +5,7 @@ import requests
 url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
 
 result = requests.get(url)
-
-print(result.text)
+doc = BeautifulSoup(result.text, "html.parser")
+# print(result.text)
+# print(doc.prettify())
+prices = doc.find_all(string="$")
