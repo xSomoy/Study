fe96e7d50d49d26f4f4465fb544d84e8d0b17d6c
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-07-15 20:31:07 +0600

    Sat, 15 Jul 2023 20:31:06 +0600

---
+++ Personal/CyberSec/myCTF/CTF00 - CCTDCF/testVersions/GNU GCC Flaw.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+
+{
+    int flag = 15435698;
+    int input;
+    printf("GNU GCC Flaw v1.0\n");
+    printf("This is an unofficial CTF challenge build by \"John Deadman\"\n");
+    printf("Social: @xSomoy\n----------------------------------------------------------\n\n");
+    printf("Hint: Find The 8 Digit Code to Get the Flag.\n\n");
+    tryAgain:
+    printf("Input The Code: ");
+    scanf("%d",&input);
+    if (input == flag)
+        printf("\nCongratulations!!! You Found The Flag\nLet Me know how you found it. DM me on social\n\n");
+    else {
+        printf("\nSorry! This Is Not The Flag\nTry Again \n");
+        goto tryAgain;
+    }
+    getch();
+}
