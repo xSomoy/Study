86214d9940bdd6f4cfda63f2cf8f4ad5b44ce698
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-10-27 01:57:56 +0600

    python porichiti

--- Python/Book-Python_Porichiti/String-Er-Bebohar.py
+++ Python/Book-Python_Porichiti/String-Er-Bebohar.py
@@ -1,3 +1,7 @@
+## BOOK: Python Porichiti - Chapter 03
+
+
+
 country = 'Bangladesh'
 print(country[0])
 print(country[1])
@@ -14,3 +18,14 @@ print(str.strip())  # Only Removes the Ending white spaces
 str1 = '  a  b c  d  '
 print(str1.lstrip())  # Only Removes the Left White Spaces
 print(str1.rstrip())  # Just Like strip()
+str2 = 'AbcDeF'
+print(str2.upper())
+print(str2.lower())
+str3 = 'Bangla'
+str4 = 'desh'
+print(str3, str4)
+str3, str4 = str4, str3
+print(str3, str4)
+
+################### 01:56 xSomoy #########################
+
