89f2e5d83deecb095a51c996a8a8dfdc8a73a2c3
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-10-27 03:38:21 +0600

    py_po c4

---
+++ Python/Book-Python_Porichiti/Lister-Songe-Porichoy.py
@@ -0,0 +1,11 @@
+## BOOK: Python Porichoy - Chapter 04
+
+li = [ 'lamp', 'laptop', 'cell phone', 'pen', 'stapler' ]
+print(type(li))
+print(len(li))
+print(li[1])
+print(li[-1])
+li2 = [ 'Bangladesh', 100, 3.5, 'Laptop']
+print(type(li2[0]))
+print(li[0:3])
+print(li[1:3])
