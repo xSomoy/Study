4ac397c70253df759f7fe58a0a8233a9d5a40184
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-10-27 23:50:02 +0600

    Fri, 27 Oct 2023 23:50:02 +0600

---
+++ Python/Book-Python_Porichiti/06_Python_Loop.py
@@ -0,0 +1,20 @@
+## BOOK: Python Porichoy - Chapter 06
+
+i = 1
+while i <= 10:
+    print (i)
+    i += 1
+
+
+# Fibbonacci Generator
+
+print('Fibbonacci Generator')
+
+fib_1, fib_2 = 0, 1
+while fib_2 < 100:
+    print(fib_2)
+    fib_1, fib_2 = fib_2, fib_1 + fib_2
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+for ch in vowels:
+    print(ch)
