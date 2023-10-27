0f16629a618060e9eef74118537e6b7ba04b731f
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-10-28 00:49:01 +0600

    Sat, 28 Oct 2023 00:49:00 +0600

--- Python/Book-Python_Porichiti/07_Data_Structure.py
+++ Python/Book-Python_Porichiti/07_Data_Structure.py
@@ -1,4 +1,7 @@
 ## BOOK: Python Porichoy - Chapter 0
+
+# List
+
 li = [1,2,3,4,5,6,7,8]
 print(li)
 li.reverse()
@@ -7,3 +10,8 @@ li.sort()
 print(li)
 li.append(9)
 print(li)
+li.insert(0, 0)
+print(li)
+li.insert(0, -1)
+print(li)
+
