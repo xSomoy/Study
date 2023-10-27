3a3b73b7dd16a52e7e01ae49ac5eb798114a8129
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-10-27 23:19:53 +0600

    py_po c5

--- Python/Book-Python_Porichiti/Conditional_Logic.py
+++ Python/Book-Python_Porichiti/Conditional_Logic.py
@@ -1,3 +1,5 @@
+## BOOK: Python Porichoy - Chapter 05
+
 print(type(True))
 print(bool(-1))
 print(bool(0))
@@ -5,3 +7,24 @@ print(bool(' '))
 print(bool('as'))
 a = True
 print(not a)
+a = 5
+b = 3
+if a > 0:
+    print('a positive')
+if b < a:
+    print('affermative')
+if a == b:
+    print('==')
+if b <= a:
+    print('<=')
+if a >= b:
+    print('>=')
+if a != b:
+    print('!=')
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+x = 'i'
+if x in vowels:
+    print(x)
+else:
+    print('not a vowels')
