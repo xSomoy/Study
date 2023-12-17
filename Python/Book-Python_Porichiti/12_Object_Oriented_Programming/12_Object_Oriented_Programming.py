9b9283af26408472e044e9939b6696025e71b90f
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-12-17 22:06:01 +0600

    C12 studying

--- Python/Book-Python_Porichiti/12_Object_Oriented_Programming/12_Object_Oriented_Programming.py
+++ Python/Book-Python_Porichiti/12_Object_Oriented_Programming/12_Object_Oriented_Programming.py
@@ -1,3 +1,26 @@
 ## BOOK: Python Porichiti - Chapter 12
 
+class Square:
+    side = 0
+    def area(self):
+        return self.side * self.side
+
+sqr1 = Square()
+area = sqr1.area()
+print(area)
+sqr1.side = 5
+area = sqr1.area()
+print(area)
+
+
+class Square:
+    def __init__(self, x):
+        self.side = x
+    def area(self):
+        return self.side * self.side
+
+sqr2 = Square(6)
+area2 = sqr2.area()
+
+print(area2)
 
