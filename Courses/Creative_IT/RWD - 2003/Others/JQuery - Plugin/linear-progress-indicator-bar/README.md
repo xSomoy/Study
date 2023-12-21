bac064dbbcbfb2d94813f48a13bfea294d41feb5
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-12-21 17:56:00 +0600

    organized

---
+++ Courses/Creative_IT/RWD - 2003/Others/JQuery - Plugin/linear-progress-indicator-bar/README.md
@@ -0,0 +1,4 @@
+# rProgressbar
+a light wet highly customize able  jQuery plugin for progress bar 
+
+#How to active rProgressbar
---
+++ Courses/Creative_IT/RWD - 2003/Others/JQuery - Plugin/linear-progress-indicator-bar/example/assets/js/active.js
@@ -0,0 +1,41 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#1abc9c'
+    });
+    $('.css').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#2ecc71'
+    });
+
+
+
+    $('.php').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#9b59b6'
+    });
+
+
+
+    $('.javascript').rProgressbar({
+        percentage: 65,
+        fillBackgroundColor: '#34495e'
+    });
+
+
+
+    $('.jquery').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#f1c40f',
+        backgroundColor: '#e67e22'
+    });
+
+
+
+
+
+
+
+})(jQuery);
